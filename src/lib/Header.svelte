9989559a0c0a069ad841f8ec8cc3d846589a63a3
Alexander J <odyssey346@disroot.org>
2022-04-22 21:07:05 +0200

    cleanup
    Signed-off-by: Alexander J <odyssey346@disroot.org>

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,18 +1,10 @@
 <script lang="ts">
-	import css from "../assets/animation.css";
 	import ProjectSegfaultLogo from "../assets/images/projectsegfault1.png";
 </script>
 <header>
-<<<<<<< HEAD
-    <img src={ProjectSegfaultLogo} id="ProjectSegfaultLogoHeader" alt="Why is this not loading? Anyway, project segfault's logo." />
-    <a href="/" class="text-flicker-in-glow" style="color: #ce1717">Project <span style="color: #00d4aa">Segfault</span></a> 
-   <section class="center">
-=======
     <img src={ProjectSegfaultLogo} id="ProjectSegfaultLogoHeader" alt="project segfault's logo." />
-    <a href="/" style="color: #ce1717">Project</a> <a href="/" style="color: #00d4aa">Segfault</a>
-	
+    <a href="/" class="text-flicker-in-glow" style="color: #ce1717">Project <span style="color: #00d4aa">Segfault</span></a> 
    <div class="center">
->>>>>>> 3d08dbd2ae052f04c710f8cb955d8bbc313399ad
     <a href="/instances" class="button">Instances</a>
     
     <a href="/gameservers" class="button">Games</a>
@@ -26,8 +18,6 @@
 </header>
 
 <style>
-
-
     header {
         background-color: #252525;
         border: solid #252525;
@@ -44,40 +34,11 @@
     .center {
 	    text-align: center;
     }
-<<<<<<< HEAD
-    .button {
-  background-color: #00755e; /* Green */
-  border: none;
-  color: white;
-  padding: 16px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  border-radius: 10px;
-  transition-duration: 0.4s;
-  cursor: pointer;
-    }
-    .button:hover {
- 	background-color: #00d4aa;
-	color: black;
-	border-radius: 2px;
-	transform: translateY(-0.25em);
-        box-shadow: 0 0.5em 0.5em -0.4em #00755e;
-    }
-    .button:active {
-	    background-color: #4beacb;
-	    box-shadow: 0 0.51em 0.51em 0.4em #00b591;
-    }
     .text-flicker-in-glow {
-	opacity:0;	    
+	    opacity:0;
     }
     .text-flicker-in-glow:hover {
-	-webkit-animation: text-flicker-in-glow 900ms both;
-	        animation: text-flicker-in-glow 900ms both;
-}
+	    -webkit-animation: text-flicker-in-glow 900ms both;
+	    animation: text-flicker-in-glow 900ms both;
+    }
 </style>
-=======
-</style>
->>>>>>> 6b6ede9b9439d9c99cdadfc28641f5c33151e600
