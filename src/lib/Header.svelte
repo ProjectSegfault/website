c9963805cfc9396abe5b098885d99fc704cb5fc6
Midou36O <facemioo4@gmail.com>
2022-05-15 16:23:32 +0100

    Two buttons for same destinations. Seriously?

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -25,9 +25,9 @@
     <a href="/" class="text-flicker-in-glow" style="color: #ce1717">Project <span style="color: #00d4aa">Segfault</span></a> 
     <a href="{'#'}" class="button" id="MenuButton" on:click={showResponsive}>Menu</a>
    <div class="center navbarpieceofshitfuckihatethis" id="navbarpieceofshitfuckihatethis">
-    <a href="https://instances.projectsegfau.lt" class="button">Instances</a>
+    <a href="https://instances.projectsegfau.lt" class="button">Instances & Games </a>
     
-    <a href="https://instances.projectsegfau.lt" class="button">Games</a>
+    
     
     <a href="/minecraft" class="button">Minecraft</a>
 
