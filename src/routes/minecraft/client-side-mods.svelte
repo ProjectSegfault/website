dd8bbbbc55f0f3b54de371cd45139b282dd71cc6
Alex J <odyssey346@disroot.org>
2022-06-21 23:12:38 +0200

    Finish i18n of Minecraft (Haven't translated to Norwegian yet.)
    Signed-off-by: Alex J <odyssey346@disroot.org>

---
+++ src/routes/minecraft/client-side-mods.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { t } from '$lib/translations';
+</script>
+<h1>{$t('common.MINECRAFTTITLE')}</h1>
+
+<h1><a href="/minecraft">{$t('common.RCS_CLICKBACK')}</a></h1>
+
+<h2>{$t('common.RCSLIST')}</h2>
+
+<p>{$t('common.RCSTHESEARE')}</p>
+
+<p>{$t('common.RCSNOTEBED')}</p>
+<ul>
+    <li><a href="https://www.curseforge.com/minecraft/mc-mods/xaeros-world-map">Xaero's World Map</a> & <a href="https://www.curseforge.com/minecraft/mc-mods/xaeros-minimap">Xaero's Minimap</a></li>
+    <li>{$t('common.RCSANYMOD')}</li>
+    <li><a href="https://www.curseforge.com/minecraft/mc-mods/simple-voice-chat">Simple Voice Chat</a></li>
+</ul>
---
+++ src/routes/minecraft/index.svelte
@@ -0,0 +1,62 @@
+<script>
+	import { t } from '$lib/translations';
+</script>
+
+<h1>{$t('common.MINECRAFTTITLE')}</h1>
+
+<h1>{$t('common.MINECRAFTDESCRIPTION')}</h1>
+
+<p>{$t('common.MINECRAFTWEHAVE')}</p>
+
+<p>{$t('common.MINECRAFTJOIN')}</p> <a href="https://discord.gg/26EG7fFtfS">{$t('common.MINECRAFTDISCORDINVITE')}</a>
+
+<h2>{$t('common.MINECRAFTRULES')}</h2>
+
+<p>{$t('common.MINECRAFTRULE1')}</p>
+<p>{$t('common.MINECRAFTRULE2')}</p>
+<p>{$t('common.MINECRAFTRULE3')}</p>
+<p>{$t('common.MINECRAFTRULE4')}</p>
+
+<h2>{$t('common.MINECRAFTIP')}</h2>
+
+<p><b>Java</b>: mc.projectsegfau.lt:25565</p>
+
+<p><b>Bedrock</b>: mc.projectsegfau.lt:19132</p>
+
+<i>{$t('common.MINECRAFTPSST1')}</i>
+
+<br>
+
+<i>{$t('common.MINECRAFTPSST2')}</i>
+
+<h2>{$t('common.MINECRAFTLINKS')}</h2>
+
+<a href="https://map.mc.projectsegfau.lt">Dynmap</a>
+
+<a href="https://apps.apple.com/us/app/mc-server-connector/id1548251304">{$t('common.MINECRAFTMCIOS')}</a>
+
+<a href="https://play.google.com/store/apps/details?id=com.smokiem.mcserverconnector&hl=no&gl=US">{$t('common.MINECRAFTMCANDROID')}</a>
+
+<h2>{$t('common.MINECRAFTRECOMMENDEDCLIENTSIDEMODS')}</h2>
+
+<a href="/minecraft/client-side-mods">{$t('common.MINECRAFTCLIENTSIDEMODSHREF')}</a>
+
+<h2>{$t('common.MINECRAFTPLUGINLISTH2')}</h2>
+
+<p>{$t('common.MINECRAFTMANUALLYMADE')}</p>
+
+<ul>
+    <li>BlockLocker</li>
+    <li>CoreProtect</li>
+    <li><a href="https://map.mc.projectsegfau.lt">Dynmap</a></li>
+    <li>EssentialsX (EssentialsXDiscord, EssentialsXGeoIP)</li>
+    <li>Geyser & Floodgate</li>
+    <li>LuckPerms</li>
+    <li>mcMMO <a href="https://dl.odyssey346.dev/Software/mcMMO_because_fuck_buying_open_source_software.jar">{$t('common.MINECRAFTOURBUILDOFMCMMO')}</a></li>
+    <li>TabTPS</li>
+    <li>Vault</li>
+    <li>ViaVersion ({$t('common.MINECRAFTVIAVERSION')})</li>
+    <li>PlugMan</li>
+    <li>CombatLogX</li>
+    <li>Simple Voice Chat</li>
+</ul>
