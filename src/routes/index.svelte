38b82d09771292ff72573b96bc8bea9c1db21bb9
Alex J <odyssey346@disroot.org>
2022-06-21 22:23:44 +0200

    Add AbuseIPDB Badge
    Signed-off-by: Alex J <odyssey346@disroot.org>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,3 +13,10 @@
 <h1> {$t('common.INDEXTELLMEMORE')} </h1>
 
 <p>{$t('common.INDEXMOREINFO')}</p>
+
+<a href="https://www.abuseipdb.com/user/82331" title="AbuseIPDB is an IP address blacklist for webmasters and sysadmins to report IP addresses engaging in abusive behavior on their networks">
+	<img src="https://www.abuseipdb.com/contributor/82331.svg" alt="AbuseIPDB Contributor Badge" style="width: 270px;background: #00d4aa; padding: 0.5rem;
+		position: absolute;
+		bottom: 8%;
+		border-radius: 16px;">
+</a>
