af11ef4c80b49e50a401a817bd9d0fb469c3d024
Akis <mavrommatisakis@gmail.com>
2022-09-29 14:17:40 +0300

    fix padding on theme toggler

--- src/lib/ThemeToggle.svelte
+++ src/lib/ThemeToggle.svelte
@@ -15,7 +15,7 @@
 
 <DarkMode bind:theme />
 
-<button on:click={toggle} class="cursor-pointer flex items-center py-1 bg-transparent border-0 font-[var(--font-primary)]">
+<button on:click={toggle} class="cursor-pointer flex items-center py-1 px-0 bg-transparent border-0 font-[var(--font-primary)]">
 	<div class="i-fa6-solid:{theme === "dark" ? "sun" : "moon"}" />
 	<span class="ml-1 sm:hidden">Toggle theme</span>
 </button>
