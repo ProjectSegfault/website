04041ecf63b87c70bff216bc11d3f42576f3d293
Akis <mavrommatisakis@gmail.com>
2022-10-01 11:46:36 +0300

    fix more issues with the theme switch (closes #83)

--- src/lib/ThemeToggle.svelte
+++ src/lib/ThemeToggle.svelte
@@ -15,7 +15,16 @@
 
 <DarkMode bind:theme />
 
-<button on:click={toggle} class="cursor-pointer flex items-center py-1 px-0 bg-transparent border-0 font-[var(--font-primary)]">
+<button on:click={toggle} class="cursor-pointer flex items-center py-1 px-0 bg-transparent border-0 font-[var(--font-primary)] color-[var(--text)]">
 	<div class="i-fa6-solid:{theme === "dark" ? "sun" : "moon"}" />
-	<span class="ml-1 sm:hidden">Toggle theme</span>
+	<span class="ml-2">Toggle theme</span>
 </button>
+
+<style>
+	@media screen and (min-width: 900px) {
+		span {
+			display: none;
+			margin-left: 0.25rem;
+		}
+	}
+</style>
