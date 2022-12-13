5ab0e2ecdceec1a97754a2253350fd186f844711
Akis <mavrommatisakis@gmail.com>
2022-12-13 17:37:58 +0200

    adjust to new api

--- src/routes/instances/+page.server.ts
+++ src/routes/instances/+page.server.ts
@@ -2,5 +2,8 @@ import type { PageServerLoad } from "./$types";
 import { env } from "$env/dynamic/private";
 
 export const load: PageServerLoad = async () => {
-    return await fetch(env.VITE_API_URL + "/api/v1/status").then((res) => res.json());
+	return {
+		state: await fetch(env.VITE_API_URL + "/api/v1/state/status").then((res) => res.json()),
+		instances: await fetch(env.VITE_API_URL + "/api/v1/status").then((res) => res.json())
+	}
 }
--- src/routes/instances/+page.svelte
+++ src/routes/instances/+page.svelte
@@ -13,52 +13,60 @@
 </svelte:head>
 
 <h1>Our instances</h1>
-<div class="flex flex-col gap-4">
-    <CardOuter>
-        <div class="wrapper">
-            {#each data.status as group}
-                <h2>{group.name}</h2>
-                <div class="items">
-                    {#each group.data as item}
-                        <CardInner
-                            title={item.name}
-                            description={item.description}
-                            icon={item.icon}
-                        >
-                            <LinksOuter>
-                                <InstanceLink url={item.link} item={item.status} type="main" />
 
-                                {#if item.us}
-                                    <InstanceLink url={item.us} item={item.statusUs} type="us" />
-                                {/if}
+{#if data.state.enabled}
+	<div class="flex flex-col gap-4">
+		<CardOuter>
+			<div class="wrapper">
+				{#each data.instances.status as group}
+					<h2>{group.name}</h2>
+					<div class="items">
+						{#each group.data as item}
+							<CardInner
+								title={item.name}
+								description={item.description}
+								icon={item.icon}
+							>
+								<LinksOuter>
+									<InstanceLink url={item.link} item={item.status} type="main" />
 
-                                {#if item.bp}
-                                    <InstanceLink url={item.bp} item={item.statusBp} type="backup" />
-                                {/if}
-                            </LinksOuter>
-                        </CardInner>
-                    {/each}
-                </div>
-            {/each}
-        </div>
-    </CardOuter>
-    
-    <span>Last updated: {dayjs
-        .unix(data.updated)
-        .format("DD/MM/YYYY HH:mm:ss")}
-    </span>
-</div>
+									{#if item.us}
+										<InstanceLink url={item.us} item={item.statusUs} type="us" />
+									{/if}
 
-<style>
-	.wrapper {
-		display: flex;
-		flex-direction: column;
-	}
+									{#if item.bp}
+										<InstanceLink url={item.bp} item={item.statusBp} type="backup" />
+									{/if}
+								</LinksOuter>
+							</CardInner>
+						{/each}
+					</div>
+				{/each}
+			</div>
+		</CardOuter>
+		
+		<span>Last updated: {dayjs
+			.unix(data.instances.updated)
+			.format("DD/MM/YYYY HH:mm:ss")}
+		</span>
+	</div>
 
-	.items {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		gap: 2rem;
-	}
-</style>
+	<style>
+		.wrapper {
+			display: flex;
+			flex-direction: column;
+		}
+
+		.items {
+			display: flex;
+			flex-direction: row;
+			flex-wrap: wrap;
+			gap: 2rem;
+		}
+	</style>
+	{:else}
+		<div class="flex items-center gap-1 mt-16">
+			<div class="i-fa6-solid:circle-info" />
+			<span>Instances are currently disabled.</span>
+		</div>
+{/if}
