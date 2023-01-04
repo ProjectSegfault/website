c4d8f3c6c6671eba35ce07c2c8d6f65aa3f0b046
Akis <mavrommatisakis@gmail.com>
2023-01-04 09:57:43 +0200

    switch to woff2 font and add post images

--- src/lib/BlogCard/Title.svelte
+++ src/lib/BlogCard/Title.svelte
@@ -4,6 +4,9 @@
 </script>
 
 {#if !isPost}
+	{#if post.feature_image}
+		<img src={post.feature_image} alt="{post.title} image" class="rounded-2">
+	{/if}
 	<a href="/blog/{post.slug}" class="text-text no-underline hover:underline"><span class="text-xl font-bold">{post.title}</span></a>
 {:else}
 	<span class="text-xl font-bold">{post.title}</span>
