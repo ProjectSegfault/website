9210bfb8f3fdb809c896a54c8a10631918adf931
Akis <mavrommatisakis@gmail.com>
2023-01-07 19:37:15 +0200

    aaa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import unoCSS from "unocss/vite";
+import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
-	plugins: [sveltekit(), unoCSS()]
+	plugins: [sveltekit(), unoCSS(), viteCommonjs()]
 });
 
 export default config;
