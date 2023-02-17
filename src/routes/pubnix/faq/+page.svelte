7b3b86b0dca762ed5a692711effa15682da92ceb
Arya Kiran <arya@projectsegfau.lt>
2023-02-17 21:51:47 +0530

    add pubnix faq

--- src/routes/pubnix/faq/+page.svelte
+++ src/routes/pubnix/faq/+page.svelte
@@ -8,6 +8,37 @@
 
 <h2>What is a pubnix?</h2>
 <span
-	>A pubnix is a [[Unix?]] server provided by a person or a group to a group
-	for non-commercial recreational goals.</span
+	>A pubnix is a Unix server provided by a person or a group to the public
+	with non-commercial recreational goals.</span
+>
+<span
+	>A pubnix is a server running UNIX or a UNIX-like operating system that is
+	opened to the public.</span
+>
+<h2>What can I do on the pubnix?</h2>
+<span
+	>Quite a lot! You can run Docker containers, set up a website, and learn
+	some amount of Linux system administration.</span
+>
+<h2>How do I get started?</h2>
+<span
+	>First, you need to register an account. You can do that <a
+		href="/pubnix/register">here</a
+	>. After registering, you need to wait a while because we manually check
+	each registration. If you get accepted, you will receive an E-mail.</span
+>
+<h2>What's your systems specs?</h2>
+<span>Guess you'll have to find out. Join today!</span>
+<h2>I joined, but I have no idea what to do.</h2>
+<span
+	>No worries! We have put out tutorials on how to do common things on our <a
+		href="https://wiki.projectsegfau.lt/index.php?title=Category%3APubnix"
+		>wiki.</a
+	></span
+>
+<h2>Do you have a place to discuss the pubnix</h2>
+<span
+	>Yep! We have a <a href="https://matrix.to/#/#pubnix:projectsegfau.lt"
+		>matrix room</a
+	> to discuss the pubnix.</span
 >
